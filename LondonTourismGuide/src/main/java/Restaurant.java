d5c4efbb444353d500fa0cb459e2c30832da3f08
harrisali97 <harris_ali97@hotmail.co.uk>
2025-01-20 10:52:59 +0000

    Add files via upload

---
+++ LondonTourismGuide/src/main/java/Restaurant.java
@@ -0,0 +1,17 @@
+// Restaurant class, a specific type of Attraction
+class Restaurant extends Attraction {
+
+    // Constructor to initialize a Restaurant
+    public Restaurant(String name, String description, String location) {
+        super(name, description, location); // Call parent class constructor
+    }
+
+    // Implement the abstract displayInfo method to show details of the restaurant
+    @Override
+    public void displayInfo() {
+        System.out.println("Restaurant: " + name);
+        System.out.println("Location: " + location);
+        System.out.println("Description: " + description);
+        System.out.println();
+    }
+}
