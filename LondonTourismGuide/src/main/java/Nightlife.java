924fc2fd1205eff6de33e7fe48f8fcfe63ec0011
harrisali97 <harris_ali97@hotmail.co.uk>
2025-01-20 12:21:36 +0000

    Update Nightlife.java

--- LondonTourismGuide/src/main/java/Nightlife.java
+++ LondonTourismGuide/src/main/java/Nightlife.java
@@ -1,7 +1,7 @@
 // Nightlife class, a specific type of Attraction
 class Nightlife extends Attraction {
 
-    // Constructor to initialize a Nightlife
+    // Constructor to initialise a Nightlife
     public Nightlife(String name, String description, String location) {
         super(name, description, location); // Call parent class constructor
     }
